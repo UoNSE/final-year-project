5344c9c291666b948fb3c097de541da7e851656f
Brendan Annable <brendan.annable@uon.edu.au>
2015-05-10 17:26:57 +1000

    FYP-139 #time 5m Center newly added items. Esc clears all items.

--- src/app/app.js
+++ src/app/app.js
@@ -8,17 +8,34 @@ define( function (require) {
 
 	var multitouch = MultiTouchManager.getInstance();
 
-	multitouch.addElementRTS($('#item'));
+	var elements = [];
 
 	$(document.body).mousedown(function(event) {
+		// prevent text selection on double click
 		event.preventDefault();
+	});
+
+	$(window).on('keyup', function (event) {
+
+		if (event.which === 27) {
+			for (var i = 0, len = elements.length; i < len; i++) {
+				elements[i].remove();
+			}
+			elements.length = 0;
+		}
+
 	});
 
 	$('#add-item').click(function () {
 
 		var element = $('<div class="item">Hello World</div>');
 		element.appendTo(document.body);
+		element.offset({
+			left: ($(window).width() - element.outerWidth()) / 2,
+			top: ($(window).height() - element.outerHeight()) / 2
+		});
 		multitouch.addElementRTS(element);
+		elements.push(element);
 
 	});
 
