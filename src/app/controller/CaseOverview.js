5006238ca96cafd50c9c0c37b65b3f7adadfdec9
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2015-05-26 22:15:18 +1000

    FYP-171 #time 25m Fix font size for case overview.

--- src/app/controller/CaseOverview.js
+++ src/app/controller/CaseOverview.js
@@ -11,19 +11,13 @@ define(function (require) {
 		template: Handlebars.compile(template),
 		styles: styles,
 		render: function () {
-			/*// Get all the buttons from the view.
+			// Get all the buttons from the view.
 			var buttons = $('.case-overview button');
-			// Iterate through every button.
-			for (var i = 0, len = buttons.length; i < len; i++) {
+			// Iterate through every button except for the last.
+			for (var i = 0, len = buttons.length - 1; i < len; i++) {
 				// Get the current button.
 				var button = $(buttons[i]);
-				// Get the html of the button.
-				var html = button.html();
-				// To get the textwidth of a button, a span must be inserted and the width can then be obtained.
-				var size = (button.html('<span>' + html + '</span>')).find('span:first').width();
-				// Revert the html and adjust the width and height accordingly.
-				button.html(html).width(size).height(size);
-			}*/
+			}
 		}
 	});
 
