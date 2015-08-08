d1a0b9c483778ad7edb5297d52b55735153e9284
Julius Sky <julius.skye@gmail.com>
2015-08-08 16:15:44 +1000

    basic VirtualPatient view elements demo. #time 3h

--- src/app/component/virtualpatient/VirtualPatientController.js
+++ src/app/component/virtualpatient/VirtualPatientController.js
@@ -7,36 +7,51 @@ define(function(require){
     'virtual-patient.css'
   ];
 
+  var virtual_patient_img_url =  'resources/images/androg.front.jpg';
+
+
   return Controller.extend({
     styles: styles,
+    // virtual_patient_img_url: virtual_patient_img_url,
+    virtual_patient_img_url:  'resources/images/androg.front.jpg',
+    selector: '#virtual-patient-img',
+
 
     events: {
       'click .case-information .title': '_onTitle',
       'click #context': '_onContext',
-      'click #background': '_onBackground'
+      'click #background': '_onBackground',
+      'click #flip-patient-button': '_flipPatient',
+      'click #question-patient-button': '_questionPatient',
+      'click #test-request-button': '_testRequests',
+      'click #test-results-button': '_testResults',
+
     },
     _onAfterRender: function () {
 
 
     },
 
-    _onTitle: function () {
-      this.back();
-    },
+    _flipPatient: function () {
 
-    _onPatientBackground: function () {
-      // TODO
-    },
+      virtual_patient_img_url:'resources/images/androg.back.jpg';
+      // $('virtual_patient_img_url'):'resources/images/androg.back.jpg';
 
-    _onContext: function () {
-      // TODO
     },
+    _questionPatient: function () {
 
-    _onBackground: function () {
-      Animate.scaleOut($('.case-information'), {
-        duration: 500,
-        easing: 'easeInBack'
-      });
+    // TODO: explode options
+
+    },
+    _testRequests: function () {
+
+    // TODO: explode options
+
+    },
+    _testResults: function () {
+
+    // TODO: explode options
+
     }
 
   });
