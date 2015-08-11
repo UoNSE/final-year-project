6cdc7089f6c69ca24340ff176fb4736d5f38fbb8
Jonathon Hope <jonathon.a.hope@gmail.com>
2015-08-12 00:00:05 +1000

    FYP-191 Initial Structure from the coding session.

---
+++ src/app/activity/goals_action/model/Action.js
@@ -0,0 +1,14 @@
+define(function (require) {
+
+    var Backbone = require('backbone');
+
+    return Backbone.Model.extend({
+
+        defaults: {
+            content: "This is an action",
+            goalId: 1
+        }
+
+    });
+
+});
