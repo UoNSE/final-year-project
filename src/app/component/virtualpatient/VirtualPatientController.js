71f92727a7dfa2c48c9a136c3ae15b71e7ebfdc0
Andrew Dabson <c3093393@uon.edu.au>
2015-08-18 14:59:39 +1000

    FYP-189 getting chart touch working

--- src/app/component/virtualpatient/VirtualPatientController.js
+++ src/app/component/virtualpatient/VirtualPatientController.js
@@ -1,106 +1,114 @@
-define(function(require){
+define(function (require) {
 
-  //TODO: make cards draggable
-  //TODO: make hotspot from data
+    //TODO: make cards draggable
+    //TODO: make hotspot from data
 
-  var $ = require('jquery');
-  var ViewController = require('controller/ViewController');
-  var Animate = require('behaviour/Animate');
+    var $ = require('jquery');
+    var glm = require('glmatrix');
+    var ViewController = require('controller/ViewController');
+    var Animate = require('behaviour/Animate');
+    var MultiTouchManager = require('behaviour/MultiTouchManager');
+    var RotateTranslateScaleBehaviour = require('behaviour/RotateTranslateScaleBehaviour');
 
-  var styles = [
-    'virtual-patient.css'
-  ];
+    var styles = [
+        'virtual-patient.css'
+    ];
 
-  var virtual_patient_img_url =  'resources/images/androg.front.jpg';
-  var context = {
-    author: {firstName: "Alan", lastName: "Johnson"},
-    body: "I Love Handlebars",
-    comments: [{
-      author: {firstName: "Yehuda", lastName: "Katz"},
-      body: "Me too!"
-    }]
-  };
+    return ViewController.extend({
 
-
-  return ViewController.extend({
-
-    collection: 'Patients',
-    styles: styles,
-    selector: '#virtual-patient-img',
+        collection: 'Patients',
+        styles: styles,
+        selector: '#virtual-patient-img',
 
 
-    events: {
-      'click #context': '_onContext',
-      'click #background': '_onBackground',
-      'click #flip-patient-button': '_flipPatient',
-      'click #question-patient-button': '_questionPatient',
-      'click #test-request-button': '_testRequests',
-      'click #test-results-button': '_testResults',
-      'click #patients-chart-button': '_showPatientsChart',
-      'click #hide-chart-button': '_hidePatientsChart',
+        events: {
+            'click #context': '_onContext',
+            'click #background': '_onBackground',
+            'click #flip-patient-button': '_flipPatient',
+            'click #question-patient-button': '_questionPatient',
+            'click #test-request-button': '_testRequests',
+            'click #test-results-button': '_testResults',
+            'click #patients-chart-button': '_showPatientsChart',
+            'click #hide-chart-button': '_hidePatientsChart',
 
-    },
-    _onAfterRender: function () {
+        },
+        _onAfterRender: function () {
+            this._addItems();
+        },
 
-    },
+        _addItems: function () {
+            //---------------------------------------------
+            var transformableResources = [
+                '<div style="background-color:red;width:100px;height:100px"></div>'
+            ]; // [a,b,c...
 
-    _onReady: function () {
-      this.listenTo(this.collection, 'add', this.render);
-      //this.collection.add({name: 'New'});
-
-      // draggables
-      $('#patients-chart-table').draggable();
-      $('#virtual-patient-img-container').draggable();
-      // $(".btn").draggable();
+            var transforms = [
+                [glm.vec3.fromValues(300, 100, 0), glm.vec3.fromValues(0.5, 0.5, 1), 0]
+            ];
+            var numItems  = transformableResources.length;
+            //var container = $('<div class')
 
 
-      $('#patients-chart-table').hide();
-      $('#hide-chart-button').hide();
 
-    },
+        },
 
-    _flipPatient: function () {
+        _onReady: function () {
+            this.listenTo(this.collection, 'add', this.render);
+            //this.collection.add({name: 'New'});
 
-      var virtual_patient_img = $('#virtual-patient-img');
-      if(virtual_patient_img.attr("src") ==="resources/images/androg.front.jpg"){
-        virtual_patient_img.attr("src", "resources/images/androg.back.jpg");
-      }
-      else{
-        virtual_patient_img.attr("src", "resources/images/androg.front.jpg");
-      }
+            // draggables
+            $('#patients-chart-table').draggable();
+            $('#virtual-patient-img-container').draggable();
+            // $(".btn").draggable();
 
-    },
-    _showPatientsChart: function() {
 
-      $('#patients-chart-table').show();
-      $('#hide-chart-button').show();
-      $('#patients-chart-button').hide();
+            $('#patients-chart-table').hide();
+            $('#hide-chart-button').hide();
 
-    },
+        },
 
-    _hidePatientsChart: function() {
+        _flipPatient: function () {
 
-      $('#patients-chart-table').hide();
-      $('#hide-chart-button').hide();
-      $('#patients-chart-button').show();
+            var virtual_patient_img = $('#virtual-patient-img');
+            if (virtual_patient_img.attr("src") === "resources/images/androg.front.jpg") {
+                virtual_patient_img.attr("src", "resources/images/androg.back.jpg");
+            }
+            else {
+                virtual_patient_img.attr("src", "resources/images/androg.front.jpg");
+            }
 
-    },
+        },
+        _showPatientsChart: function () {
 
-    _questionPatient: function () {
+            $('#patients-chart-table').show();
+            $('#hide-chart-button').show();
+            $('#patients-chart-button').hide();
 
-    // TODO: explode options
+        },
 
-    },
-    _testRequests: function () {
+        _hidePatientsChart: function () {
 
-    // TODO: explode options
+            $('#patients-chart-table').hide();
+            $('#hide-chart-button').hide();
+            $('#patients-chart-button').show();
 
-    },
-    _testResults: function () {
+        },
 
-    // TODO: explode options
+        _questionPatient: function () {
 
-    }
+            // TODO: explode options
 
-  });
-});
+        },
+        _testRequests: function () {
+
+            // TODO: explode options
+
+        },
+        _testResults: function () {
+
+            // TODO: explode options
+
+        }
+
+    }); //end return function to send to viewController
+});   //end require
