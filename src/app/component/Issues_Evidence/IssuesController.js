95f4357238c425c97926bf114023fe031d3d95f2
Dylan Barnett <c3164484@uon.edu.au>
2015-08-18 12:53:18 +1000

    FYP-192 #time 1h added basic merge functionality change page structure slightly

--- src/app/component/Issues_Evidence/IssuesController.js
+++ src/app/component/Issues_Evidence/IssuesController.js
@@ -65,14 +65,49 @@ define(function (require) {
             //check collisions
             //get btn pos
             var pos=jQuery(event.target).offset();
+            //check for card deletion
             var delbtn = $( "#delbtn").offset();
             var deldist = Math.sqrt(Math.abs(Math.pow((delbtn.left - pos.left),2) + Math.pow((delbtn.top - pos.top),2)));
             //console.log(deldist);
             if(deldist<32){
                 (event.target).remove();
-
             }
-            
+            //check for card merging
+            var list;
+
+            if($(event.target).hasClass("issue")){
+                //check all evidence cards
+                list = $('#evidences').children();
+            }
+            else {
+                //check all issue cards
+                list = $('#issues').children();
+            }
+
+            for(var i=0;i<list.length;i++){
+                //div
+                var card = $(list[i]);
+                if(!card.hasClass("merged")){
+                    var cardpos = card.offset();
+                    var mergeDist = Math.sqrt(Math.abs(Math.pow((cardpos.left - pos.left),2) + Math.pow((cardpos.top - pos.top),2)));
+                    if(mergeDist<32){
+                        //add "merged" class to div
+                        card.addClass("merged");
+                        //update colour
+                        card.addClass("btn-material-orange");
+                        //update card text
+                        if($(event.target).hasClass("issue")){
+                            card.html($(event.target).text()+ " | " + card.text());
+                        }
+                        else {
+                            card.html(card.text()+ " | " + $(event.target).text());
+                        }
+                        //remove old card
+                        (event.target).remove();
+                    }
+                }
+            }
+
             //hide menu
             $( "#menu" ).hide();
         }
