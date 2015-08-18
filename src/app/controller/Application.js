2dbaadad6e8a2150bb2c4b82fb3448f4ae736fb9
Brendan Annable <brendan.annable@uon.edu.au>
2015-08-19 03:20:39 +1000

    Disable text selection with CSS rather than JS.

--- src/app/controller/Application.js
+++ src/app/controller/Application.js
@@ -32,24 +32,6 @@ define(function (require) {
 			});
 
 			this._bindEvents();
-		},
-
-		_bindEvents: function () {
-			$(document.body).on('mousedown', this._onBodyMouseDown.bind(this));
-			$(window).on('keyup', this._onKeyUp.bind(this));
-		},
-
-		_onBodyMouseDown: function (event) {
-			event.preventDefault();
-		},
-
-		_onKeyUp: function (event) {
-			if (event.which === 27) { // ESC key
-				// Stop all videos from playing/leaking.
-				this.elements.filter('video').trigger('pause').attr('src', '');
-				this.elements.remove();
-				this.elements = $();
-			}
 		}
 
 	});
