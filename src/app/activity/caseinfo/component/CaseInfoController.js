7b7460c3b8204136aab1e3611291ef2885ec65ca
Ben <Benjamin.Atkinson@uon.edu.au>
2015-08-25 17:02:51 +1000

    Timer added to activity

--- src/app/activity/caseinfo/component/CaseInfoController.js
+++ src/app/activity/caseinfo/component/CaseInfoController.js
@@ -8,6 +8,41 @@ define(function (require) {
     var ViewController = require('controller/ViewController');
     var Animate = require('behaviour/Animate');
 
+    var clockTimeout = 100;
+    var activityTimer = 0;
+
+
+    function convertTimer(milli){
+        var milliSecs = milli;
+
+        var msSecs = (1000);
+        var msMins = (msSecs * 60);
+        var msHours = (msMins * 60);
+        var numHours = Math.floor(milliSecs/msHours);
+        var numMins = Math.floor((milliSecs - (numHours * msHours)) / msMins);
+        var numSecs = Math.floor((milliSecs - (numHours * msHours) - (numMins * msMins))/ msSecs);
+
+
+
+        if (numSecs < 10){
+            numSecs = "0" + +numSecs;
+        }
+        if (numMins < 10){
+            numMins = "0" + +numMins;
+        }
+
+        if (numHours < 10){
+            numHours = "0" + +numHours;
+        }
+
+        return numHours + ":" + numMins + ":" + numSecs;
+    }
+
+    function updateClock(){
+        activityTimer += clockTimeout;
+        $('#activity-clock').text(convertTimer(activityTimer));
+    }
+
     return ViewController.extend({
         //hide back button
         displayBack: false,
@@ -21,19 +56,23 @@ define(function (require) {
             'click .btn-kept':'restoreCard'
         },
 
+        onReady:function(){
+         setInterval(updateClock,clockTimeout);
+        },
+
         keepCard : function(event) {
             $(event.target).removeClass('btn-keep').addClass('btn-kept').parents('.well').animate({
                 opacity: 0.5,
-                top: "+=250",
+                top: "+=500",
             }, 500, null);
         },
 
         restoreCard : function(event) {
             $(event.target).removeClass('btn-kept').addClass('btn-keep').parents('.well').animate({
                 opacity: 1,
-                top: "-=250",
+                top: "-=500",
             }, 500, null);
-        }
+        },
 
     });
 });
