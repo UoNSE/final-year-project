2a1b39de6b9277c9e13b22b6b2dfce3cabf314a6
Ben <Benjamin.Atkinson@uon.edu.au>
2015-08-25 15:20:11 +1000

    Toggle card status with Keep button behaviour

--- src/app/activity/caseinfo/component/CaseInfoController.js
+++ src/app/activity/caseinfo/component/CaseInfoController.js
@@ -17,14 +17,23 @@ define(function (require) {
 
         //events
         events: {
-            'click .btn-keep':'keepCard'
+            'click .btn-keep':'keepCard',
+            'click .btn-kept':'restoreCard'
         },
 
         keepCard : function(event) {
-            $(event.target).parents('.well').animate({
-                opacity: 0.25,
-                top: "+=50",
-            }, 2000, null);
+            $(event.target).removeClass('btn-keep').addClass('btn-kept').parents('.well').animate({
+                opacity: 0.5,
+                top: "+=250",
+            }, 500, null);
+        },
+
+        restoreCard : function(event) {
+            $(event.target).removeClass('btn-kept').addClass('btn-keep').parents('.well').animate({
+                opacity: 1,
+                top: "-=250",
+            }, 500, null);
         }
+
     });
 });
