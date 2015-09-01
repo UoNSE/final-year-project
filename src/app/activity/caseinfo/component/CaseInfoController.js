375de890a367c41d0e775d3c70e6be0d1ba40d6c
Ben <Benjamin.Atkinson@uon.edu.au>
2015-09-01 16:27:31 +1000

    FYP-231 #time 3h - Added timer effects - Selectable reveal - Data restructure

--- src/app/activity/caseinfo/component/CaseInfoController.js
+++ src/app/activity/caseinfo/component/CaseInfoController.js
@@ -8,9 +8,40 @@ define(function (require) {
     var ViewController = require('controller/ViewController');
     var Animate = require('behaviour/Animate');
 
-    var clockTimeout = 1000;
-    var activityTimer = 0;
+    var CLOCK_TIMEOUT = 1000;
+    var CLOCK_MID = 600000;
+    var CLOCK_LOW = 300000;
+    var ACTIVITY_TIMER = 0;
+    var issues = {};
+    var hiddenCards;
+    var sCounter = 0;
 
+    function issueTrack(){
+        //track individual issues that are revealed to pass to Issues/Evidence activity
+    }
+
+    function showHidden(text) {
+        if ((text.attr('issue')) != null){
+            ++sCounter;
+            $('.hidden-info').each(function() {
+                if ($(this).attr('threshold') <= sCounter){
+                    $(this).removeClass('.hidden-info').fadeIn(2500);
+                }
+            });
+        }
+
+    }
+
+    function init() {
+        hiddenCards = $('.hidden-info').css('display', 'none');/*.each(function() {
+            //hidden.push($(this).attr('id'));
+        });*/
+        var times = ($('#activity-clock').text()).split(":");
+        ACTIVITY_TIMER += +times[0]*60*60;
+        ACTIVITY_TIMER += +times[1]*60;
+        ACTIVITY_TIMER += +times[2];
+        ACTIVITY_TIMER *= 1000;
+    }
 
     function convertTimer(milli){
         var milliSecs = milli;
@@ -33,8 +64,15 @@ define(function (require) {
     }
 
     function updateClock(){
-        activityTimer += clockTimeout;
-        $('#activity-clock').text(convertTimer(activityTimer));
+        ACTIVITY_TIMER -= CLOCK_TIMEOUT;
+        if (ACTIVITY_TIMER < CLOCK_LOW) {
+            $('#activity-clock').addClass('activity-clock-low');
+            CLOCK_LOW = -1;
+        } else if (ACTIVITY_TIMER < CLOCK_MID){
+            $('#activity-clock').addClass('activity-clock-mid');
+            CLOCK_MID = -1;
+        }
+        $('#activity-clock').text(convertTimer(ACTIVITY_TIMER));
     }
 
     return ViewController.extend({
@@ -56,7 +94,8 @@ define(function (require) {
         },
 
         onReady: function () {
-            setInterval(updateClock, clockTimeout);
+            setInterval(updateClock, CLOCK_TIMEOUT);
+            init();
         },
 
         keepCard: function (event) {
@@ -79,12 +118,15 @@ define(function (require) {
             if (!(item.hasClass('inv-list-item'))) {
                 if (item.hasClass('selected-text')){
                     $("#list-"+item.attr('id')).remove();
+                    --sCounter;
                 } else {
-                    item.clone().attr('id','list-'+item.attr('id')).addClass('inv-list-item').appendTo($('.inventory').find('ul'));
+                    showHidden(item);
+                    item.clone().attr('id','list-'+item.attr('id')).addClass('inv-list-item well').appendTo($('.inventory').find('ul'));
                 }
             } else {
                 var id = item.attr('id');
                 item.remove();
+                --sCounter;
                 item = $("#"+(id.slice(5,id.length)));
             }
             item.toggleClass('selected-text');
