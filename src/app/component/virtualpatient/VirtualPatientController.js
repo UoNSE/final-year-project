b503eb8dcbacdad2f3552f20da2bd4c6ed43a979
Andrew Dabson <c3093393@uon.edu.au>
2015-09-08 18:28:47 +1000

    FYP-232 #time 45m re-implementing hotspot code onto original branch rather than continue with defective 232..._subviews branch

--- src/app/component/virtualpatient/VirtualPatientController.js
+++ src/app/component/virtualpatient/VirtualPatientController.js
@@ -79,6 +79,22 @@ define(function(require){
 
             this._hideElements();
             this._startEventFeed();
+            this.addHotSpots();
+        },
+
+        addHotSpots: function() {
+            $('#eve').click(function(e) { //e is the event that get passed to anonymous functions by click()
+                var divCoords = $(this).offset();
+
+                var relX = e.pageX - divCoords.left;          // get mouse coords relative to
+                var relY = e.pageY - divCoords.top;
+                relX *= 100 / $(this).width();                // convert to percent
+                relY *= 100 / $(this).height();
+
+                relX = relX.toFixed(2);
+                relY = relY.toFixed(2); //round
+                console.log(relX + '% : ' + relY+'%');
+            });
         },
 
         _hideElements: function() {
