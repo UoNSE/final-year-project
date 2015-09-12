7d9b6f3760bab7ebe9b9be812f017b6d0a445fc3
Andrew Dabson <c3093393@uon.edu.au>
2015-09-12 18:54:38 +1000

    #FYP-261 #time 1h 15m Dynamic positioning of hotspots to patient data

--- src/app/component/activity/virtualpatient/patientbody/PatientBody.js
+++ src/app/component/activity/virtualpatient/patientbody/PatientBody.js
@@ -4,15 +4,34 @@ define(function (require) {
     var Component = require('core/Component');
     var template = require('text!component/activity/virtualpatient/patientbody/PatientBody.hbs'); //text!
     var ActionButton = require('component/actionbutton/ActionButton'); //referencing javascript file (require js)
+    var Hotspots = require('collection/Hotspots'); //HotspotData from db.json, collections, model
 
     return Component.extend({
         template: template,
         classes: ['patient-body'],
         styles: 'component/activity/virtualpatient/patientbody/PatientBody.css',
+        collection: { //we say the component has a collection (collection of models)
+            hotspots: new Hotspots() //, newCollection: new Newvalue()
+
+        },
+        events: {
+            'click .vpb': 'logPos'
+        },
 
         initialize: function () {
             Component.prototype.initialize.apply(this, arguments);
 
+            this.listenTo(this.collection.hotspots, 'sync', this.onHotspots.bind(this));
+            //THE onHotspots() method ADDS THE HOTSPOTS
+            //add a listener (listening to sync (backbone thing))
+            //checks the collection has fully loaded
+
+            this.collection.hotspots.fetch();
+            //linking to the collection: { hotpots } key
+            //if you dont call fecth(), the collection will always be empty (fetch calls ajax event under the hood)
+
+
+            /*
             var button = [];
             for (var i = 0; i < 5; i++) {
                 button[i] = new ActionButton({
@@ -25,27 +44,51 @@ define(function (require) {
 
                 button[i].position.set(i * 30, i * 30);
                 button[i].scale.set(.5, .5);
-                button.rotation = i * Math.PI / 4.
+                button.rotation = i * Math.PI / 4;
                 this.add(button[i]);
             }
+            */
 
             //button.position.set(100, 100);
             //button.rotation = Math.PI/4.; ----- this.scale.set(1,1); //scales the virtual patient COMPONENT
         },
 
-        addHotSpots: function () {
-            $('#eve').click(function (e) { //e is the event that get passed to anonymous functions by click()
-                var divCoords = $(this).offset();
+        onHotspots: function (collection) {
+            // this is the callback function
 
-                var relX = e.pageX - divCoords.left;          // get mouse coords relative to
-                var relY = e.pageY - divCoords.top;
-                relX *= 100 / $(this).width();                // convert to percent
-                relY *= 100 / $(this).height();
+            collection.each(function (model) {
+                var button = new ActionButton({
+                    model: {
+                        text: '',
+                        color: 'red',
+                        classes: ['hotspotbutton']
+                    }
+                });
 
-                relX = relX.toFixed(2);
-                relY = relY.toFixed(2); //round
-                console.log(relX + '% : ' + relY + '%');
-            });
+                var x = model.get('x');
+                var y = model.get('y');
+                button.position.set(x, y);
+                button.scale.set(.5, .5);
+                this.add(button);
+            }, this);
+
+        },
+
+        logPos: function (e) {
+
+            // page basis: *--->
+            //             |
+            //             v
+
+            var x = e.pageX; //mouse relative to page
+            var y = e.pageY;
+            var X = $(window).width(); //total page width/height $(this).width() also works?
+            var Y = $(window).height();
+            var relX =  x - X/2;
+            var relY = -y + Y/2; //because we're flipping the Y vector also
+
+
+            console.log(relX + ' ' + relY);
         }
 
 
---
+++ src/app/model/Hotspot.js
@@ -0,0 +1,16 @@
+define(function (require) {
+
+	var Backbone = require('backbone');
+
+	return Backbone.Model.extend({
+
+		defaults: {
+			//name: 'Hotspot',
+			patientId: null,
+			x: 0,
+			y: 0
+		}
+
+	});
+
+});
