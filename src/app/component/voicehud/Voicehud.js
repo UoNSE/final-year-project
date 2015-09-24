7f4884d96599e9e4b1dba5d5ed1af744bbf18201
Julius Sky <julius.skye@gmail.com>
2015-09-24 19:01:24 +1000

    Added dummy progresses.

--- src/app/component/voicehud/Voicehud.js
+++ src/app/component/voicehud/Voicehud.js
@@ -21,20 +21,35 @@ define(function (require) {
 
 			var numGroups = 5;
 			var timelines = [];
+			this.dummygroupprogressscore = [0,3,2,3,2];
 			for(var i = 0; i<5; i++){
-				this.groupProgressTimeline = this.addTimeline();
+				this.groupProgressTimeline = this.addTimeline(i);
 				this.add(this.groupProgressTimeline);
-				if(i>0){
-					var yPosition = this.groupProgressTimeline.position.y;
-					this.groupProgressTimeline.position.set(1,yPosition-i*70);
-				}
-				// timelines.push(groupProgressTimeline);
-				// timelines.position.set(100,100);
-				// this.add(timelines);
-				var xPosition = this.groupProgressTimeline.position.x;
-				var yPosition = this.groupProgressTimeline.position.y;
 
-				this.groupProgressTimeline.position.set(xPosition+100,yPosition+150);
+				// offset the timelines
+				if(i>0){
+					var yPosition = this.position.y;
+					this.groupProgressTimeline.position.set(0,yPosition-i*70);
+				}
+
+				// position each timeline
+				var xPosition = this.position.x;
+				var yPosition = this.groupProgressTimeline.position.y;
+				// this.groupProgressTimeline.position.set(xPosition+100,yPosition+150);
+
+
+				this.groupProgressTimeline.position.x = xPosition+100;
+				this.groupProgressTimeline.position.y = yPosition+150;
+
+				// // scale timeline buttons
+				// this.groupProgressTimeline.scale.set(0.5,0.5);
+				//
+				// // scale distance between buttons
+				// for(var i=0; i<this.groupProgressTimeline.children.length; i++){
+				// 	var child = this.groupProgressTimeline.children[i];
+				// 	var positionX = child.position.x;
+				// 	child.position.set(positionX * 0.5, 1);
+				// }
 
 				// this.add(groupProgressTimeline);
 				var groupNum = i+1;
@@ -49,13 +64,29 @@ define(function (require) {
 
 		},
 
-		addTimeline: function(){
+		// randGroupState: function(){
+		// 	// var randomBool = Math.random() >= 0.5;
+		// 	// return randomBool;
+		// 	return Math.floor(Math.random() * 4) + 1;
+		// },
+		//
+		stageCompleteBool: function(teststage, progress){
+			if(progress >= teststage){
+				return true;
+			}
+			else{
+				return false;
+			}
+		},
+
+		addTimeline: function(i){
+			var progress = this.dummygroupprogressscore[i];
 			var timeline = new Timeline({
 				collection: new ActionButtons([
-					{text: 'Case Information'},
-					{text: 'Identify Issues'},
-					{text: 'Goals and Actions', disabled: true},
-					{text: 'Reflection', disabled: true}
+					{text: 'Case Info', disabled: this.stageCompleteBool(4,progress)},
+					{text: 'Identify Issues',disabled: this.stageCompleteBool(3,progress)},
+					{text: 'Goals Actions', disabled: this.stageCompleteBool(2,progress)},
+					{text: 'Reflect', disabled: this.stageCompleteBool(1,progress)}
 				]),
 				class: "timeline"
 			});
