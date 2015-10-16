498d8974d9454200ec906acda83b2245641debbd
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2015-10-16 17:42:11 +1100

    no message

--- src/app/component/overview/Overview.js
+++ src/app/component/overview/Overview.js
@@ -1,37 +1,29 @@
 define(function (require) {
 	'use strict';
 
-	var Component = require('core/Component');
-	var template = require('text!component/overview/Overview.hbs');
+	let Component = require('core/Component');
+	let template = require('text!component/overview/Overview.hbs');
 
-	var ActionButtons = require('collection/ActionButtons');
-	var Timeline = require('component/timeline/Timeline');
-	var Hint = require('component/hint/Hint');
+	let ActionButton = require('model/ActionButton');
+	let Timeline = require('component/timeline/Timeline');
+	let Hint = require('component/hint/Hint');
 
 	return Component.extend({
 
 		template: template,
 		styles: 'case-overview.css',
 
-		initialize: function () {
+		initialize: function (overview, params) {
 			Component.prototype.initialize.apply(this, arguments);
 
 			this.add(new Timeline({
-				collection: new ActionButtons([
-					{text: 'Case Information', href: 'cases/' + this.model.id + '/information'},
-					{text: 'Identify Issues', href: this.getActivityLink('issues'), disabled: true},
-					{text: 'Goals and Actions', href: this.getActivityLink('goals'), disabled: true},
-					{text: 'Reflection', href: this.getActivityLink('reflection'), disabled: true}
-				])
+				model: overview
 			}));
+
 			var hint = this.add(new Hint({
 				model: {text: 'Tap an activity below'}
 			}));
 			hint.position.y = 120;
-		},
-
-		getActivityLink: function (name) {
-			return 'cases/' + this.model.id + '/activity/' + name;
 		}
 
 	});
--- src/app/component/timeline/Timeline.js
+++ src/app/component/timeline/Timeline.js
@@ -12,33 +12,61 @@ define(function (require) {
 
 		initialize: function (width, height) {
 			Component.prototype.initialize.apply(this, arguments);
-			let buttons = this.collection;
-			let n = buttons.size();
+			let buttons = this.model.get('buttons');
 			let distanceBetween = 200;
 
-			for (let i = 0; i < n; i++) {
-				if (i < n - 1) {
-					let line = new Rectangle({
-						model: new RectangleModel({
-							width: distanceBetween,
-							height: 5,
-							color: '#333'
-						})
-					});
-					let scale = i - (n - 2) / 2;
-					line.position.set(scale * distanceBetween, 0);
-					this.add(line);
-				}
-			}
+			this.addLines(buttons.size(), distanceBetween);
+			this.addButtons(buttons, distanceBetween);
 
+			this.listenTo(this.model, 'change', this.onChange.bind(this));
+			this.model.set('complete', 0);
+		},
+
+		addLines: function (size, width) {
+			for (let i = 0; i < size - 1; i++) {
+				let line = new Rectangle({
+					model: new RectangleModel({
+						width: width,
+						height: 5,
+						color: '#333'
+					})
+				});
+				let scale = i - (size - 2) / 2;
+				line.position.set(scale * width, 0);
+				this.add(line);
+			}
+		},
+
+		addButtons: function (buttons, distanceBetween) {
+			let size = buttons.size();
 			buttons.each(function (model, i) {
 				let button = new ActionButton({
 					model: model
 				});
-				let scale = i - (n - 1) / 2;
+				let scale = i - (size - 1) / 2;
 				button.position.set(scale * distanceBetween, 0);
 				this.add(button);
 			}, this);
+		},
+
+		onChange: function (model) {
+			let complete = model.get('complete');
+			let buttons = model.get('buttons');
+			for (let i = 0, len = complete; i < len; i++) {
+				let button = buttons.at(i);
+				if (button.get('disabled')) {
+					button.set('disabled', false);
+				}
+				if (button.get('color') === 'primary') {
+					button.set('color', 'success');
+				}
+			}
+
+			if (complete < buttons.size()) {
+				buttons.at(complete).set('disabled', false);
+			}
+
 		}
+
 	});
 });
