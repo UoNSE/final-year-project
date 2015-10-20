687d4879f6aa67b2711894124325e288b87c1712
Jonathon Hope <jonathon.a.hope@gmail.com>
2015-10-20 15:16:23 +1100

    Fix card removal.

--- src/app/component/activity/casebackground/backgroundinfo/CaseBackground.js
+++ src/app/component/activity/casebackground/backgroundinfo/CaseBackground.js
@@ -61,14 +61,14 @@ define(function (require) {
         }()),
 
         template: template,
-        collection:{
-            info : new InfoCollection(),
-            infogroup : new InfoCollection()
+        collection: {
+            info: new InfoCollection(),
+            infogroup: new InfoCollection()
         },
         /**
          * The current screen number, corresponds to the 'groupId' property in db.
          */
-        round:0,
+        round: 0,
         /**
          * Case id of current dataset
          */
@@ -76,22 +76,26 @@ define(function (require) {
         /**
          * The sidebar (original position, can be moved) of collected pieces of relevant information.
          */
-        infogroup:null,
+        infogroup: null,
         /**
          * A count of the number of relevant pieces of information shown on this screen
          */
-        relevantpieces:0,
+        relevantpieces: 0,
         /**
          * Rules based card matching.
          */
         cardMatcher: new CardMatcher(),
+        /**
+         * array of cards
+         */
+        infoCards: [],
 
-        initialize: function (caseID,roundID) {
+        initialize: function (caseID, roundID) {
             Component.prototype.initialize.apply(this, arguments);
             this.round = +roundID;
             this.caseID = +caseID;
             this.registerRules();
-            this.setupFixedComponents(caseID,roundID);
+            this.setupFixedComponents(caseID, roundID);
             this.setupActivityStartState();
         },
 
@@ -110,10 +114,10 @@ define(function (require) {
                 if (info.get('relevant')) {
 
                     let match = new InfoModel({
-                        'infoid':info.get('id'),
-                        'content':info.get('content'),
-                        'short':info.get('short'),
-                        'relevant':info.get('relevant')
+                        'infoid': info.get('id'),
+                        'content': info.get('content'),
+                        'short': info.get('short'),
+                        'relevant': info.get('relevant')
 
                     });
 
@@ -122,6 +126,7 @@ define(function (require) {
                     // remove cards components as they will be
                     // replaced by match components
                     infoCard.remove();
+
                 } else infoCard.shake();
 
             }.bind(this));
@@ -129,8 +134,8 @@ define(function (require) {
             this.cardMatcher.addRule('Info => InfoGroup', InfoInfoGroupMatchRule);
         },
 
-        setupFixedComponents: function (caseID,roundID) {
-            let nextRound = +roundID +1;
+        setupFixedComponents: function (caseID, roundID) {
+            let nextRound = +roundID + 1;
             // add help component to the page
             this.help = this.add(new Help({
                 model: new HelpModel({body: HelpText})
@@ -144,7 +149,7 @@ define(function (require) {
                     color: 'light-green',
                     classes: 'help-btn actions-btn',
                     icon: 'content-send',
-                    href: 'cases/'.concat(caseID, '/activity/case-information/')+nextRound+ '/info'
+                    href: 'cases/'.concat(caseID, '/activity/case-information/') + nextRound + '/info'
                 }
             }));
 
@@ -168,9 +173,9 @@ define(function (require) {
 
             //set up the info collection pane
             this.infogroup = this.addInfoGroup()
-            this.infogroup.position.set(MatchPositioning.x(),0);
+            this.infogroup.position.set(MatchPositioning.x(), 0);
             //attach collection to the sub-collection within the collected info pane
-            this.infogroup.model.set('info',infoGroupCollection);
+            this.infogroup.model.set('info', infoGroupCollection);
 
 
             // Listen to the sync events on both collections, which waits for
@@ -189,39 +194,45 @@ define(function (require) {
          */
         onInfoSync: function (info) {
             this.relevantpieces = this.infogroup.model.get('info').length;
-            let rows = 3;
+            let rows = 4;
             let rowSpacing = 200;
-            let colSpacing = this.width/2 + 20;
+            let colSpacing = this.width / 2 + 20;
+
+            let context = this;
             /*
              filter - all info cards NOT from this group
              filter - all info cards already collected
              then add the rest of the cards to the screen
              * */
             info.filter((info) => this.round === info.get('groupId'))
-                .filter((info) => !(this.collection.infogroup.find((rInfo) => rInfo.get('infoid')=== info.get('id'))))
-                .forEach(function (model, i){
+                .filter((info) => !(this.collection.infogroup.find((rInfo) => rInfo.get('infoid') === info.get('id'))))
+                .forEach(function (model, i) {
                     //increment the number of relevant pieces to match to determine completion
-                    if (model.get('relevant')){this.relevantpieces++;}
+                    if (model.get('relevant')) {
+                        this.relevantpieces++;
+                    }
                     //assign model attributes
-                    Object.assign(model.attributes,{
-                        width : this.width,
+                    Object.assign(model.attributes, {
+                        width: this.width,
                         id: model.get('id'),
                         title: 'Case Information',
-                        body : model.get('content'),
-                        short : model.get('short'),
-                        color : 'pink'
+                        body: model.get('content'),
+                        short: model.get('short'),
+                        color: 'pink'
                     });
                     let card = this.addInfo(model);
                     let x = (i < rows ? -colSpacing : colSpacing );
 
-                    card.position.set(x,((+(i%rows)-1)*rowSpacing));
-                },this);
+                    context.infoCards.push(card);
 
-            let finalPage = info.filter((info) => (this.round+1) === info.get('groupId')).length;
+                    card.position.set(x, ((+(i % rows) - 1) * rowSpacing));
+                }, this);
+
+            let finalPage = info.filter((info) => (this.round + 1) === info.get('groupId')).length;
             // if there is no info items in the next group, change the hidden link
             // to point back to the top level case information menu
-            if (finalPage == 0){
-                this.hiddenLink.model.href = 'cases/'+this.caseID+'/information';
+            if (finalPage == 0) {
+                this.hiddenLink.model.href = 'cases/' + this.caseID + '/information';
                 this.hiddenLink.render();
                 this.hiddenHint.model.text = "Congratulations!<br> you've found all the pieces of case information.<br> Touch the GREEN button to continue";
                 this.hiddenHint.render();
@@ -231,19 +242,19 @@ define(function (require) {
 
         },
 
-        onAddInfoGroup: function(){
+        onAddInfoGroup: function () {
             // remove body text and re-render component to show new items in info collection
-            this.infogroup.model.set('body','');
+            this.infogroup.model.set('body', '');
             this.infogroup.render();
         },
 
         addInfoGroup: function () {
             let model = new InfoGroupModel({
-                    height:600,
-                    width:400,
-                    title:'Information',
-                    body:'Drag infomation here to mark it as relevant',
-                    color:'orange'
+                    height: 600,
+                    width: 400,
+                    title: 'Information',
+                    body: 'Drag infomation here to mark it as relevant',
+                    color: 'orange'
                 }
             );
             return this.createDraggableCard(model, InfoGroupCard);
@@ -262,9 +273,11 @@ define(function (require) {
             return card;
         },
 
-        checkComplete : function(){
-            if(this.infogroup.model.get('info').length >= this.relevantpieces)
-            {
+        checkComplete: function () {
+            if (this.infogroup.model.get('info').length >= this.relevantpieces) {
+                this.infoCards.forEach((card)=>{
+                    card.remove();
+                });
                 this.hiddenLink.show();
                 this.hiddenHint.show();
             }
